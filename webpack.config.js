1638c4d9c12b98f0a1f6c02e1b3bcb5a5d02b5d9
Evgene Popolamov <popolamov908@gmail.com>
2020-11-20 20:45:23 +0300

    feat: finish framework

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const isLoaders = () => {
       loader: "babel-loader",
       options: {
         presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-proposal-class-properties"],
       },
     },
   ];
